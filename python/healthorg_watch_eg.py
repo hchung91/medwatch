75157aec4a41f8a8afd08e288060b96a7fb944ad
Hugh Chung <h.chung91@gmail.com>
2020-08-01 19:00:23 -0400

    Incorporated keyword bug fix into medwatch.py and example file

--- python/healthorg_watch_eg.py
+++ python/healthorg_watch_eg.py
@@ -29,7 +29,7 @@ while True:
     organizations = ['FDA', 'WHO', 'HHS']
     gov_domains = ['https://fda.gov', 'https://who.int', 'https://hhs.gov']
     gov_urls = ['https://www.fda.gov/news-events/fda-newsroom/press-announcements', 'https://www.who.int/news-room/releases', 'https://www.hhs.gov/coronavirus/news/index.html']
-    keywords_ignore = [['Daily Roundup'], [''], ['Daily Roundup']]
+    ignorewords = ['Daily Roundup']
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
@@ -56,12 +56,9 @@ while True:
         if datetime.now() < time_check:
             pause.until(time_check)
 
-        email_msg = []
-        email_organizations = []
-
         keywords = mw.load_keywords_csv(keywords_dir)
 
-        for org, url, domain, kw_ignore in zip(organizations, gov_urls, gov_domains, keywords_ignore):       
+        for org, url, domain in zip(organizations, gov_urls, gov_domains):       
             print('\n----------------------------------')
             print(f'[{mw.now_hms()}] Checking {org}')
 
@@ -97,33 +94,39 @@ while True:
                 old_body = old_soup.find('body')
                 old_anchors = mw.get_anchors(old_soup)
                 diff_anchors = mw.get_new_diff(anchors, old_anchors)
-                diff_hrefs, diff_contents = mw.parse_anchors(diff_anchors)
                 
-                if len(diff_anchors) > 0:
+                rel_anchors, rel_keywords = mw.relevant_anchors(diff_anchors, keywords=keywords, ignorewords=ignorewords)
+                
+                if len(rel_anchors) > 0:
+                    email_msg = []
+
                     message = f'[{current_time}] New links found: \n---------------\n'
                     mw.write_log(message, url)
 
-                    rel_anchors, rel_keywords = mw.check_for_keywords(diff_anchors, keywords, keywords_ignore = kw_ignore)
-                    rel_hrefs, rel_contents = mw.parse_anchors(rel_anchors)
+                    email_msg.append(f'\n[{current_time}] {org} - {url}')
+                    email_msg.append('------------------------')
+
+                    for rel_anchor, rel_kws in zip(rel_anchors, rel_keywords):
+                        rel_href, rel_content = mw.parse_anchor(rel_anchor)
+                        message=f'{rel_content} :: {mw.href_to_link(rel_href, [url, domain])}'
+                        email_msg.append(message)
+                        mw.write_log(message, url)
+
 
                     if len(rel_anchors) > 0:
                         email_organizations.append(org)
                         email_msg.append(f'\n[{current_time}] {org} - {url}')
                         email_msg.append('------------------------')
-                        
-                        all_keywords_found = set([item for sublist in rel_keywords for item in sublist if item != ''])
-                        keywords_found = ', '.join(all_keywords_found)
-                        message=f'Links related to {keywords_found}:'
+                        tmp_list = ', '.join(rel_kws)
+                        message=f'Link marked because of keywords: {tmp_list}'
+                        email_msg.append(message)
                         mw.write_log(message, url)
-                        
-                        for rel_href, rel_content, anchor_keywords in zip(rel_hrefs, rel_contents, rel_keywords):
-                            message=f'{rel_content} :: {mw.href_to_link(rel_href, [url, domain])}'
-                            email_msg.append(message)
-                            mw.write_log(message, url)
-                            tmp_list = ', '.join(anchor_keywords)
-                            message=f'Link marked because of keywords: {tmp_list}'
-                            email_msg.append(message)
-                            mw.write_log(message, url)
+
+                        receive_addresses = mw.get_listserv(listserv_dir)
+                        email_msg.insert(0, f'New links related to the following keywords have been detected! \n{keywords}\n')
+                        email_msg.insert(0, f'Subject: Medwatch update from {org} [{mw.now_hms()}]\n\n')
+                        email_msg = u'\n'.join(email_msg).encode('utf-8')
+                        mw.send_email_notification(email_msg, receive_addresses, sendercreds_dir)
 
                     else:
                         message=f'[{current_time}] New links not related to {keywords}\n'
@@ -143,19 +146,6 @@ while True:
         
        
         print('-----------------\nSweep Complete')
-
-        if len(email_msg) > 0:
-            receive_addresses = mw.get_listserv(listserv_dir)
-
-            email_msg.insert(0, f'New links related to the following keywords have been detected! \n{keywords}\n')
-            
-            email_organizations = ', '.join(email_organizations)
-            email_msg.insert(0, f'Subject: Medwatch update from {email_organizations} [{mw.now_hms()}]\n\n')
-
-            email_msg = u'\n'.join(email_msg).encode('utf-8')
-
-            mw.send_email_notification(email_msg, receive_addresses, sendercreds_dir)
-
 
         now = datetime.now()
 
