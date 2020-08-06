7100d366e068021a2ce4cf994d3663cfe1cb8a89
Hugh Chung <h.chung91@gmail.com>
2020-08-05 23:18:47 -0400

    Fixed indentation error but relevant anchors not being caught. Added lines to help debug

--- python/main_eg.py
+++ python/main_eg.py
@@ -36,7 +36,7 @@ while True:
     starttime, endtime = mw.gen_start_end_times(start_time=start_hms, end_time=end_hms)
 
     now = datetime.now()
-    
+
     if now < starttime:
         print(f"Will start at {starttime} EST")
         pause.until(starttime)
@@ -60,102 +60,106 @@ while True:
             readCSV = csv.reader(csvfile, delimiter=',')
             next(readCSV)
 
-        for row in readCSV:
-            co, yco, sym, exch, mkcap, size, am, url_home, url_pr = df.iloc[ii].T.values
+            for row in readCSV:
+                co, yco, sym, exch, mkcap, size, am, url_home, url_pr = row
 
-            co = co.strip()
-            yco = yco.strip()
-            sym = sym.strip()
-            exch = exch.strip()
-            url_home = url_home.strip()
-            url_pr = url_pr.strip()
+                co = co.strip()
+                yco = yco.strip()
+                sym = sym.strip()
+                exch = exch.strip()
+                url_home = url_home.strip()
+                url_pr = url_pr.strip()
 
-            # Skip if entry is n/a for some reason
-            if mw.is_na(url_pr):
+                # Skip if entry is n/a for some reason
+                if mw.is_na(url_pr):
+                    print("\n----------------------------------")
+                    timestamp = datetime.now()
+                    timestamp = timestamp.strftime('%a, %d %b %Y %H:%M:%S')
+                    print(f"[{timestamp}] Skipping {co}")
+                    print("----------------------------------\n")
+                    continue
+
+                # Otherwise, proceed
                 print("\n----------------------------------")
-                timestamp = datetime.now()
-                print(f"[{timestamp}] Skipping {co}")
-                print("----------------------------------\n")
-                continue
+                print(f"Checking {yco}")
 
-            # Otherwise, proceed
-            print("\n----------------------------------")
-            print(f"Checking {yco}")
-
-            try:
-                r = requests.get(url_pr, headers=HEADERS)
-            except:
-                message = f"CONNECTION FAILED! - Status Code: {r.status_code}\n"
-                print(f"\n{message}")
-                mw.write_log(message, url_pr)
-                continue
-
-            page = r.text
-            page = " ".join(page.split())
-            soup = BeautifulSoup(page, "html.parser")
-            body = soup.find("body")
-
-            if not body:
-                message = f"Cannot find <body></body> for {url_pr}"
-                mw.write_log(message, url_pr)
-                continue
-
-            # Check if page is different from cached page
-            has_update = mw.cache_updated(url_pr, page)
-            current_time = mw.now_hms()
-
-            # print(has_update)
-            print("\n")
-
-            if has_update:
-                # Get new anchors
-                anchors = mw.get_anchors(soup)
-
-                # Load old page/anchors
-                old_page = mw.load_cache(url_pr)
-                old_soup = BeautifulSoup(old_page, "html.parser")
-                old_body = old_soup.find("body")
-                old_anchors = mw.get_anchors(old_soup)
-
-                # List all new anchors
-                diff_anchors = mw.get_new_diff(anchors, old_anchors)
-
-                rel_anchors, rel_keywords = mw.relevant_anchors(diff_anchors, keywords=keywords)
-
-                if len(rel_anchors) > 0:
-                    email_msg = []
-
-                    message = f'[{current_time}] New links found: \n---------------\n'
+                try:
+                    r = requests.get(url_pr, headers=HEADERS)
+                except:
+                    message = f"CONNECTION FAILED! - Status Code: {r.status_code}\n"
+                    print(f"\n{message}")
                     mw.write_log(message, url_pr)
+                    continue
 
-                    email_msg.append(f'\n[{current_time}] {yco} - {url_home}')
-                    email_msg.append('------------------------')
+                page = r.text
+                page = " ".join(page.split())
+                soup = BeautifulSoup(page, "html.parser")
+                body = soup.find("body")
 
-                    for rel_anchor, rel_kws in zip(rel_anchors, rel_keywords):
-                        rel_href, rel_content = mw.parse_anchor(rel_anchor)
-                        message=f'{rel_content} :: {mw.href_to_link(rel_href, [url_pr, domain])}'
-                        email_msg.append(message)
-                        mw.write_log(message, url_pr)
-
-                        tmp_list = ', '.join(rel_kws)
-                        message=f'Link marked because of keywords: {tmp_list}'
-                        email_msg.append(message)
-                        mw.write_log(message, url_pr)
-                        email_msg.append('\n----\n')
-
-                    receive_addresses = mw.get_listserv(DIR_LISTSERV)
-                    email_msg.insert(0, f'New links related to the following keywords have been detected! \n{keywords}\n')
-                    email_msg.insert(0, f'Subject: Medwatch update from {org} [{mw.now_hms()}]\n\n')
-                    email_msg = u'\n'.join(email_msg).encode('utf-8')
-                    mw.send_email_notification(email_msg, receive_addresses, DIR_SENDCREDS)
-                    
-                else:
-                    message = f'[{current_time}] Update detected but no new anchors'
+                if not body:
+                    message = f"Cannot find <body></body> for {url_pr}"
                     mw.write_log(message, url_pr)
+                    continue
+
+                # Check if page is different from cached page
+                has_update = mw.cache_updated(url_pr, page)
+                current_time = datetime.now()
+                current_time = current_time.strftime('%a, %d %b %Y %H:%M:%S')
                     
-                    
-                mw.store_cache(url_pr, page)
-                print('\n\n')
+
+                # print(has_update)
+                print("\n")
+
+                if has_update:
+                    # Get new anchors
+                    anchors = mw.get_anchors(soup)
+
+                    # Load old page/anchors
+                    old_page = mw.load_cache(url_pr)
+                    old_soup = BeautifulSoup(old_page, "html.parser")
+                    old_body = old_soup.find("body")
+                    old_anchors = mw.get_anchors(old_soup)
+
+                    # List all new anchors
+                    diff_anchors = mw.get_new_diff(anchors, old_anchors)
+
+                    rel_anchors, rel_keywords = mw.relevant_anchors(diff_anchors, keywords=keywords)
+
+                    if len(rel_anchors) > 0:
+                        email_msg = []
+
+                        message = f'[{current_time}] New links found: \n---------------\n'
+                        mw.write_log(message, url_pr)
+                        mw.write_log(str(rel_anchors), url_pr)
+
+                        email_msg.append(f'\n[{current_time}] {yco} - {url_home}')
+                        email_msg.append('------------------------')
+
+                        for rel_anchor, rel_kws in zip(rel_anchors, rel_keywords):
+                            rel_href, rel_content = mw.parse_anchor(rel_anchor)
+                            message=f'{rel_content} :: {mw.href_to_link(rel_href, [url_pr, domain])}'
+                            email_msg.append(message)
+                            mw.write_log(message, url_pr)
+
+                            tmp_list = ', '.join(rel_kws)
+                            message=f'Link marked because of keywords: {tmp_list}'
+                            email_msg.append(message)
+                            mw.write_log(message, url_pr)
+                            email_msg.append('\n----\n')
+
+                        receive_addresses = mw.get_listserv(DIR_LISTSERV)
+                        email_msg.insert(0, f'New links related to the following keywords have been detected! \n{keywords}\n')
+                        email_msg.insert(0, f'Subject: Medwatch update from {org} [{mw.now_hms()}]\n\n')
+                        email_msg = u'\n'.join(email_msg).encode('utf-8')
+                        mw.send_email_notification(email_msg, receive_addresses, DIR_SENDCREDS)
+                        
+                    else:
+                        message = f'[{current_time}] Update detected but no new anchors'
+                        mw.write_log(message, url_pr)
+                        
+                        
+                    mw.store_cache(url_pr, page)
+                    print('\n\n')
 
         print("-----------------\nSweep Complete")
 
